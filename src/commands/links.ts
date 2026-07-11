23d4439b9469dd4fd0a7c245b08a315ce78aa3a6
DimoEdgard <20609098+DimoEdgrad@users.noreply.github.com>
2026-07-11 15:12:58 +0330

    Update links.ts

--- src/commands/links.ts
+++ src/commands/links.ts
@@ -4,8 +4,8 @@ export const links = () => async (ctx: Context) => {
   await ctx.reply(
     '🌐 لینک‌های مورد نظر خود را از منوی زیر انتخاب کنید:',
     Markup.inlineKeyboard([
-      [Markup.button.url('گیت‌هاب پروژه', 'https://github.com/DimoEdgrad/telegram-bot-vercel-boilerplate')],
-      [Markup.button.url('کانال تلگرام', 'https://t.me/YourChannel')] // آیدی کانال خودت را بذار
+      [Markup.button.url('ارتباط با سازنده ربات', 'https://t.me/dimoedgard')],
+      [Markup.button.url('کانال تلگرام', 'https://t.me/TheGhostButterfly')] // آیدی کانال خودت را بذار
     ])
   );
 };
