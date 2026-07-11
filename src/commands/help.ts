e0e4322a9f371e040bbe14e94a11d72288c28d43
DimoEdgard <20609098+DimoEdgrad@users.noreply.github.com>
2026-07-11 15:26:07 +0330

    Update help.ts

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -2,12 +2,12 @@ import { Context } from 'telegraf';
 
 export const help = () => async (ctx: Context) => {
   const helpText = 
-    `📜 *راهنمای دستورات ربات:*\n\n` +
-    `🔹 /start \\- شروع مجدد ربات\n` +
-    `🔹 /help \\- نمایش همین راهنما\n` +
-    `🔹 /about \\- درباره این پروژه\n` +
-    `🔹 /links \\- لینک‌های مفید و شبکه‌های اجتماعی\n` +
-    `🔹 /creator \\- ارتباط با سازنده ربات`;
+    `📜 راهنمای دستورات ربات:\n\n` +
+    `🔹 /start - شروع مجدد ربات\n` +
+    `🔹 /help - نمایش همین راهنما\n` +
+    `🔹 /about - درباره این پروژه\n` +
+    `🔹 /links - لینک‌های مفید و شبکه‌های اجتماعی\n` +
+    `🔹 /creator - ارتباط با سازنده ربات`;
   
-  await ctx.replyWithMarkdownV2(helpText);
+  await ctx.reply(helpText);
 };
