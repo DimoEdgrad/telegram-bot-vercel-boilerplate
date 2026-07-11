d5eb444a48733fed31767cc5021b21dd66c7127e
DimoEdgard <20609098+DimoEdgrad@users.noreply.github.com>
2026-07-11 15:25:49 +0330

    Update start.ts

--- src/commands/start.ts
+++ src/commands/start.ts
@@ -3,6 +3,6 @@ import { Context } from 'telegraf';
 export const start = () => async (ctx: Context) => {
   const firstName = ctx.from?.first_name || 'کاربر گرامی';
   await ctx.reply(
-    `سلام *${firstName}* عزیز\\! 🚀\nبه ربات پیشرفته من خوش آمدید\\.\n\nبرای دیدن لیست کارهای من، دستور /help را ارسال کنید\\.`
+    `سلام ${firstName} عزیز! 🚀\nبه ربات پیشرفته من خوش آمدید.\n\nبرای دیدن لیست کارهای من، دستور /help را ارسال کنید.`
   );
 };
