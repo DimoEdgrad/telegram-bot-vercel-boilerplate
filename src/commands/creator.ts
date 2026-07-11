79e91c7a4c3152084d9ee0e5b237dbf8b01920a0
DimoEdgard <20609098+DimoEdgrad@users.noreply.github.com>
2026-07-11 15:17:06 +0330

    Update creator.ts

--- src/commands/creator.ts
+++ src/commands/creator.ts
@@ -4,7 +4,7 @@ export const creator = () => async (ctx: Context) => {
   await ctx.reply(
     '👤 برای ارتباط مستقیم با سازنده و پشتیبانی ربات، از دکمه زیر استفاده کنید:',
     Markup.inlineKeyboard([
-      [Markup.button.url('ارتباط با من', 'https://t.me/dimoedgard')] // آیدی خودت را بدون @ بذار
+      [Markup.button.url('ارتباط با من', 'https://t.me/PouyanBakhshi')] // آیدی خودت را بدون @ بذار
     ])
   );
 };
