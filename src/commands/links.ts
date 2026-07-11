ccf3d257dde21fc65d3a6bf192d711eeb4e85f5d
DimoEdgard <20609098+DimoEdgrad@users.noreply.github.com>
2026-07-11 15:16:47 +0330

    Update links.ts

--- src/commands/links.ts
+++ src/commands/links.ts
@@ -4,7 +4,7 @@ export const links = () => async (ctx: Context) => {
   await ctx.reply(
     '🌐 لینک‌های مورد نظر خود را از منوی زیر انتخاب کنید:',
     Markup.inlineKeyboard([
-      [Markup.button.url('ارتباط با سازنده ربات', 'https://t.me/dimoedgard')],
+      [Markup.button.url('ارتباط با سازنده ربات', 'https://t.me/PouyanBakhshi')],
       [Markup.button.url('کانال تلگرام', 'https://t.me/TheGhostButterfly')] // آیدی کانال خودت را بذار
     ])
   );
